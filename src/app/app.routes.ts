1c37adfa161a10074d36cb1b9e58d058e83566b9
Arti-Khillare1 <arti.k@pacewisdom.com>
2024-10-23 16:33:30 +0530

    remove default landing page with shikshagraha route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,7 @@ import { LandingPageComponent } from './landing-page.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/shikshagraha', pathMatch: 'full' }, 
-  {
-    path: 'shikshagraha',
-    component: LandingPageComponent
-  },
+  { path: '', redirectTo: '/', pathMatch: 'full' }, 
   {
     path: 'shikshagraha/:action/:id',
     component: LandingPageComponent,
